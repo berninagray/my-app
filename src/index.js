6a7ab5132c32253132215d556ecb0be2730ba96f
berninagray <43260023+berninagray@users.noreply.github.com>
2020-11-27 19:05:03 -0700

    changed renderSquare method in board

--- src/index.js
+++ src/index.js
@@ -12,8 +12,9 @@ class Square extends React.Component {
 
   render() {
     return (
-      <button className="square"
-      onClick={() => this.setState(value: 'X'})}
+      <button
+        className="square"
+        onClick={() => this.setState(value: 'X'})}
       >
         {this.state.value}
       </button>
@@ -29,7 +30,12 @@ class Board extends React.Component {
     };
   }
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return (
+    <Square
+      value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
